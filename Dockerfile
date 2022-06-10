7503dd6142fbf61b232026e1421c7afccee9f07a
hojun <hoojoun@gmail.com>
2022-06-10 22:59:08 +0900

    error 1

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,9 @@ WORKDIR /usr/src/app
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 
+RUN apk update
+RUN apk add postgresql-dev gcc python3-dev musl-dev zlib-dev jpeg-dev
+
 COPY . /usr/src/app/
 
 RUN pip install --upgrade pip
--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,7 @@ urlpatterns=[
     path('update_comment/<int:pk>/',views.CommentUpdate.as_view()),
     path('update_post/<int:pk>/',views.PostUpdate.as_view()),
     path('create_post/',views.PostCreate.as_view()),
-    path('tag/<str:slug>/', views.tag_page),
+    #path('tag/<str:slug>/', views.tag_page),
     path('category/<str:slug>/', views.category_page),\
     path('',views.PostList.as_view()),
     path('<int:pk>/new_comment/',views.new_comment),
